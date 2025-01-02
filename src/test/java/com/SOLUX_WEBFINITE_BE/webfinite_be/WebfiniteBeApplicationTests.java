5c2b31c536720b607ffa84fc58669cc8792f16ff
yezixx <skylow917@daum.net>
2025-01-02 18:15:12 +0900

    Feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/SOLUX_WEBFINITE_BE/webfinite_be/WebfiniteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SOLUX_WEBFINITE_BE.webfinite_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfiniteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
