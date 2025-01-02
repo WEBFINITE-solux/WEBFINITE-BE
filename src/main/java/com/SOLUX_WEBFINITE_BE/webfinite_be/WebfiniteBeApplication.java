9279d6030693ccf233592c9ce73bbbcfeaaf33d4
yezixx <skylow917@daum.net>
2025-01-02 18:15:12 +0900

    Feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/WebfiniteBeApplication.java
@@ -0,0 +1,13 @@
+package com.SOLUX_WEBFINITE_BE.webfinite_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfiniteBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfiniteBeApplication.class, args);
+	}
+
+}
