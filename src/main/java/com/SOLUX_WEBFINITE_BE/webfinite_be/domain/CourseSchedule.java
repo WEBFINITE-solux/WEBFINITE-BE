1e940f34aa76430cb2813f2b35520456914db51b
yezixx <skylow917@daum.net>
2025-01-05 15:29:48 +0900

    Feat: Setter 설정 - Setter 사용하는 게 더 편해서 오픈해두었으나 다른 방법 찾으면 닫을 예정 - Day를 String으로 받아서 ENUM 타입으로 변환해주기 위해 Setter 직접 작성

--- src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/domain/CourseSchedule.java
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/domain/CourseSchedule.java
@@ -2,11 +2,12 @@ package com.SOLUX_WEBFINITE_BE.webfinite_be.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalTime;
 
 @Entity
-@Getter
+@Getter @Setter
 public class CourseSchedule {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "schedule_id")
@@ -27,6 +28,29 @@ public class CourseSchedule {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "course_id")
     private Course course;
+
+    // setter 직접 세팅
+    // String -> Enum 타입인 Day로 변환
+    public void setDay(String day) {
+        switch (day) {
+            case "MON":
+                this.day = Day.MON; break;
+            case "TUE":
+                this.day = Day.TUE; break;
+            case "WED":
+                this.day = Day.WED; break;
+            case "THU":
+                this.day = Day.THU; break;
+            case "FRI":
+                this.day = Day.FRI; break;
+            case "SAT":
+                this.day = Day.SAT; break;
+            case "SUN":
+                this.day = Day.SUN; break;
+            default:
+                throw new IllegalArgumentException("Invalid day: " + day);
+        }
+    }
 
     // ==== 연관 관계 메서드 ====
     // 로직 상에서 양쪽에 값을 세팅해주기 위함
