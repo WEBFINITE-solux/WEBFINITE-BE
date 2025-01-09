77e902d9003935fa90cdb014438a7c664296ac6a
yezixx <skylow917@daum.net>
2025-01-09 20:05:01 +0900

    Fix: 브랜치 재세팅 - 관련 없는 파일 지우려고 domain 아래 패키지 지웠었는데 이걸 다른 브랜치에서 pull 하면 해당 브랜치에서도 domain 아래 파일들 전부 지워짐...

---
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/common/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.SOLUX_WEBFINITE_BE.webfinite_be.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+/**
+ * (ex) NOT_EXISTS_USER_ID(HttpStatus.NOT_FOUND, "U-001", "존재하지 않는 유저 아이디입니다."),
+ * 위 예와 같이 에러코드들 Enum으로 관리
+ * 형식: errorName(HttpStatus, "errorCode", "message");
+ * 에러코드는 관련 기능 이름 첫번째 알파벳-번호 형식으로 작성
+ * */
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // U - user 관련 에러 코드
+    NOT_EXISTS_USER_ID(HttpStatus.NOT_FOUND, "U-001", "존재하지 않는 유저 아이디입니다."),
+
+    // C - Course 관련 에러 코드
+    NOT_EXISTS_Course_ID(HttpStatus.NOT_FOUND, "C-001", "존재하지 않는 강의 아이디입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String errorCode; // 커스텀 에러 코드 -> http status code만으로는 정학한 원인 파악이 어렵기 때문
+    private final String message;
+}
