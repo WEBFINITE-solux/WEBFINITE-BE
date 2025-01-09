dbcc1eb42ca8de94109062d592f264b4c1681140
yezixx <skylow917@daum.net>
2025-01-09 18:15:03 +0900

    Feat: File 정보 담을 DTO 생성

---
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/dto/FileDTO.java
@@ -0,0 +1,19 @@
+package com.SOLUX_WEBFINITE_BE.webfinite_be.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FileDTO {
+    @JsonProperty("course_id")
+    private Long courseId;
+    @JsonProperty("file_id")
+    private Long fileId;
+    @JsonProperty("original_filename")
+    private String originalFilename;
+    @JsonProperty("file_path")
+    private String filePath;
+    private String message;
+}
