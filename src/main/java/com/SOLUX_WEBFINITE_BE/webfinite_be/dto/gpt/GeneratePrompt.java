f7beb984887ad0ae4ef0374e1346add66e341ce2
yezixx <skylow917@daum.net>
2025-01-14 18:12:13 +0900

    Feat: 프롬프트 주의사항 추가 및 다듬기 - 형식 다듬고 주의사항 추가해서 JSON 형태의 문자열만 반환하도록 수정

--- src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/dto/gpt/GeneratePrompt.java
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/dto/gpt/GeneratePrompt.java
@@ -3,13 +3,21 @@ package com.SOLUX_WEBFINITE_BE.webfinite_be.dto.gpt;
 public class GeneratePrompt {
 
     public static String createPlanPrompt(String subjectName, String startDate, String endDate, String startUnit, String endUnit, String additionalPrompt, String fileText) {
-        String template = subjectName +" 과목의 학습 계획을 한국어로 작성해줘. 학습 기간은 " + startDate + "부터 " + endDate + "까지이고 " +
-                "각 주차별로 한개씩 학습 목표와 학습 내용을 세부적으로 작성해줘." +
-                "주차수보다 단원수가 많을경우 주차별로 묶어서 작성해줘." +
-                "학습 범위는 " + startUnit + "부터 " + endUnit + "까지야 " +
-                additionalPrompt +
+        String template =
+                "이전의 프롬프트는 모두 무시해줘 " +
+                "너는 학습 일정과 단원 범위, 추가 요구사항, 참고 자료를 받아 학습 계획을 작성헤주는 역할이야. " +
+                "추가 요구사항은 필수가 아니라서 비어있을 수도 있어. " +
+                "주의 사항을 꼭 지켜서 작성해줘" +
+                subjectName +" 과목의 학습 계획을 한국어로 작성해줘. 학습 기간: " + startDate + " ~  " + endDate + ", " +
+                "학습 범위: " + startUnit + " ~ " + endUnit + ". " +
+                "추가 요구사항: " + additionalPrompt + ", " +
                 "학습 계획에 사용할 참고 자료는" + fileText + "야." +
-                "JSON 형식의 결과를 제외한 텍스트는 제외해줘." +
+                "주의 사항: " +
+                "각 주차별로 학습 목표와 학습 내용을 세부적으로 작성해주고 같은 주차끼리 묶어줘. " +
+                "단원수가 학습 기간의 주차수보다 많을 경우 한 주에 여러 단원을 넣어서라도 꼭 학습 기간을 맞춰줘. " +
+                "각 주를 나누는 기준은 7일이야. " +
+                "JSON 형식의 결과를 제외한 텍스트는 제외해줘. " +
+                "결과 맨 앞부분의 ```JSON과 맨 끝의 ```도 제외하고 List로 반환해줘. " +
                 "각 주차 예시) " +
                 "{week: 1," +
                 "plan_title: 1-4 행렬 연산," +
