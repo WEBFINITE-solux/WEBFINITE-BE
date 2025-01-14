e0fc678aabf1fbde171db0ec4da2462415baaa1c
yezixx <skylow917@daum.net>
2025-01-14 18:10:01 +0900

    Feat: PlanDTO 클래스 생성 - 이후 학습 계획 조회 기능에 사용할듯

---
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/dto/PlanDTO.java
@@ -0,0 +1,33 @@
+package com.SOLUX_WEBFINITE_BE.webfinite_be.dto;
+
+import com.SOLUX_WEBFINITE_BE.webfinite_be.domain.LearningPlan;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PlanDTO {
+    private String promptText;
+    private List<Plan> learningPlan;
+
+    @Data
+    @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    private class Plan {
+        private Long planId;
+        private int week;
+        private String planTitle;
+        private String planDescription;
+    }
+
+    public List<Plan> toPlanDTO(List<LearningPlan> learningPlans) {
+        return learningPlans.stream().map(learningPlan -> new Plan(learningPlan.getId(), learningPlan.getWeek(), learningPlan.getTitle(), learningPlan.getDescription())).toList();
+    }
+}
