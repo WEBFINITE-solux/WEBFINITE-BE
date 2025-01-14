6f99e344297d7e504271404d589692e1101c41ec
yezixx <skylow917@daum.net>
2025-01-14 18:11:22 +0900

    Feat: 반환 타입 수정 및 리팩토링 - JsonProperty 대신 JsonNaming으로 어노테이션 하나로 처리가능하도록 수정

--- src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/controller/PlanController.java
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/controller/PlanController.java
@@ -2,17 +2,17 @@ package com.SOLUX_WEBFINITE_BE.webfinite_be.controller;
 
 import com.SOLUX_WEBFINITE_BE.webfinite_be.service.GPTService;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.service.PlanService;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/plan")
@@ -22,25 +22,20 @@ public class PlanController {
     private final GPTService gptService;
 
     @PostMapping("/{courseId}/new")
-    public String createPlan(@PathVariable("courseId") Long courseId, @RequestBody CreatePlanRequest request) throws IOException, URISyntaxException {
+    public Map<String, String> createPlan(@PathVariable("courseId") Long courseId, @RequestBody CreatePlanRequest request) throws IOException, URISyntaxException {
         return planService.createPlan(courseId, request.promptText, request.startDate, request.endDate, request.startUnit, request.endUnit, request.fileId);
     }
 
 
     @Data
     @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     static class CreatePlanRequest {
-        @JsonProperty("prompt_text")
         private String promptText;
-        @JsonProperty("start_date")
         private LocalDate startDate;
-        @JsonProperty("end_date")
         private LocalDate endDate;
-        @JsonProperty("start_unit")
         private String startUnit;
-        @JsonProperty("end_unit")
         private String endUnit;
-        @JsonProperty("file_id")
         private Long fileId;
     }
 }
