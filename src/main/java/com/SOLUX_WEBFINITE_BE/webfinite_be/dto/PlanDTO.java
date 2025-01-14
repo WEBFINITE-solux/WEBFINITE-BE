b73cb8bc190a4ea4bc46aad91c1fe730328738fd
yezixx <skylow917@daum.net>
2025-01-14 21:07:43 +0900

    Refactor: 내부 클래스 및 메서드 static으로 변경

--- src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/dto/PlanDTO.java
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/dto/PlanDTO.java
@@ -20,14 +20,14 @@ public class PlanDTO {
     @Data
     @AllArgsConstructor
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-    private class Plan {
+    public static class Plan {
         private Long planId;
         private int week;
         private String planTitle;
         private String planDescription;
     }
 
-    public List<Plan> toPlanDTO(List<LearningPlan> learningPlans) {
+    public static List<Plan> toPlanDTO(List<LearningPlan> learningPlans) {
         return learningPlans.stream().map(learningPlan -> new Plan(learningPlan.getId(), learningPlan.getWeek(), learningPlan.getTitle(), learningPlan.getDescription())).toList();
     }
 }
