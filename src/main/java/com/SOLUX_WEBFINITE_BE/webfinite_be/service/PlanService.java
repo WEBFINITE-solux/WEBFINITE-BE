3c95420592a64b9fc26b4187d70aa28dc1fd211f
yezixx <skylow917@daum.net>
2025-01-14 18:09:12 +0900

    Feat: 반환 타입 수정 및 DB 저장 로직 추가 - Json 문자열 파싱에서 LearningPlan 엔티티와 매핑 - 메시지만 반환하도록 수정

--- src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/service/PlanService.java
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/service/PlanService.java
@@ -2,11 +2,16 @@ package com.SOLUX_WEBFINITE_BE.webfinite_be.service;
 
 import com.SOLUX_WEBFINITE_BE.webfinite_be.domain.Course;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.domain.CourseFile;
+import com.SOLUX_WEBFINITE_BE.webfinite_be.domain.LearningPlan;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.domain.Prompt;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.dto.gpt.GeneratePrompt;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.exception.CourseNotFoundException;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.reposiroty.CourseRepository;
 import com.SOLUX_WEBFINITE_BE.webfinite_be.reposiroty.PlanRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -15,6 +20,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -25,7 +33,7 @@ public class PlanService {
 
     private final GPTService gptService;
 
-    public String createPlan(Long courseId, String promptText, LocalDate startDate, LocalDate endDate, String startUnit, String endUnit, Long fileId) throws IOException {
+    public Map<String, String> createPlan(Long courseId, String promptText, LocalDate startDate, LocalDate endDate, String startUnit, String endUnit, Long fileId) throws IOException {
         Course course = courseRepository.findById(courseId).orElseThrow(() -> new CourseNotFoundException());
         CourseFile courseFile = courseRepository.findFileById(fileId).orElseThrow(() -> new IllegalStateException("파일이 존재하지 않습니다."));
         String filePath = courseFile.getFilePath();
@@ -38,9 +46,18 @@ public class PlanService {
 
         String requestPrompt = GeneratePrompt.createPlanPrompt(course.getTitle(), startDate.toString(), endDate.toString(), startUnit, endUnit, promptText, getText);
 
+        String response = gptService.chat(requestPrompt);
+
+        List<LearningPlan> plans = textToPlan(response);
+
+        for(LearningPlan plan : plans){
+            plan.setCourse(course);
+            planRepository.savePlan(plan);
+        }
+
         planRepository.savePrompt(prompt);
 
-        return gptService.chat(requestPrompt);
+        return Map.of("message", "학습 계획 생성 완료");
     }
 
     private String pdfToText(File file) throws IOException {
@@ -51,5 +68,20 @@ public class PlanService {
         }catch (IOException e){
             throw new IOException("파일을 읽을 수 없습니다.");
         }
+    }
+
+    private List<LearningPlan> textToPlan (String text){
+        List<LearningPlan> plans = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try{
+            plans = objectMapper.readValue(text, new TypeReference<List<LearningPlan>>() {});
+            return plans;
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
