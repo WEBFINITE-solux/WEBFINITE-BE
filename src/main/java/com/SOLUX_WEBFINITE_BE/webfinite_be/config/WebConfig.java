94c81a064d9d49b4332ee30277b0d6d95ca5d764
yezixx <skylow917@daum.net>
2025-01-19 15:17:23 +0900

    Fix: 쿠키 인증 부분 주석 처리 - 테스트시 오류 발생해서 주석처리 해놓음

--- src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/config/WebConfig.java
+++ src/main/java/com/SOLUX_WEBFINITE_BE/webfinite_be/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
-                .allowCredentials(true); // 쿠키 인증 요청 허용
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE"); // 허용할 HTTP method
+                //.allowCredentials(true); // 쿠키 인증 요청 허용
     }
 }
